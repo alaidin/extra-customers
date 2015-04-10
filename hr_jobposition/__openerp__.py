b7ca35b35ec42efea3979814576d491d6ade976a
alaidin <alaidin@alaidin-VirtualBox>
2015-04-10 17:21:57 +0200

    hr_jobposition 1.0

---
+++ hr_jobposition/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name' : 'is_HRJobposition',
+    'summary' : 'Job position update',
+    'version' : '0.1',
+    'author' : 'IS',
+    'category': 'Human Resources',
+    'description' : """
+    Add Ref. To job position
+    """,
+    'website': 'iris-solutions.fr',
+    'depends' : ['hr'],
+    'data': [
+#         'security/account_security.xml',
+#         'security/ir.model.access.csv',       
+          'hr_view.xml', 
+    ],
+    'installable': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
